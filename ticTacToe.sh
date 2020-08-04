8891d798e9046e6ee2c5f4b482b75a3f9ce9cd7b
“teji1998” <tejasvinirpk@gmail.com>
2020-08-05 04:08:27 +0530

    Added:Functionality for the resetting of the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
 echo "WELCOME TO THE TIC-TAC-TOE GAME."
+
+#Declaring an array
+declare -a board
+
+#Function for resetting the board
+boardReset()
+{
+	for (( position=1 ; position<=9 ; position++ ))
+	do
+		board[$position]=" - "
+	done
+	echo "The resetting of board is done."
+}
+boardReset
+
