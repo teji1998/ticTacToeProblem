adeb2f8de3e20af7407279db54ca1ba06de7c567
“teji1998” <tejasvinirpk@gmail.com>
2020-08-05 05:02:00 +0530

    Added:Functionality to decide who will play first

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,17 +2,31 @@
 
 echo "WELCOME TO THE TIC-TAC-TOE GAME."
 
+#Length of grid
+LENGTH_OF_GRID=9
 #Declaring an array
 declare -a board
 
 #Function for resetting the board
 boardReset()
 {
-	for (( position=1 ; position<=9 ; position++ ))
+	for (( position=1 ; position<=LENGTH_OF_GRID ; position++ ))
 	do
 		board[$position]=" - "
 	done
 	echo "The resetting of board is done."
 }
-boardReset
 
+#Function to decide who will play first
+playFirstToss()
+{
+	toss=$(( $RANDOM % 2 ))
+	if [ $toss -eq 1 ]
+	then
+		echo "You have won the toss.You will play first."
+	else
+		echo "Computer has won the toss.Computer will play first."
+	fi
+}
+boardReset
+playFirstToss
