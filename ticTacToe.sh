98ea34ac431602748d6ee350ecb8c9f6447bfa00
“teji1998” <tejasvinirpk@gmail.com>
2020-08-05 06:25:22 +0530

    Added:Functionality to choose letter by player and computer

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -17,6 +17,38 @@ boardReset()
 	echo "The resetting of board is done."
 }
 
+#To choose X or 0 by player
+choosePlayerLetter()
+{
+	read -p "Enter 0 for 'X' and 1 for '0': " letter
+	if [ $letter -eq 1 ]
+	then
+		playerLetter='0'
+		computerLetter='X'
+	elif [ $letter -eq 0 ]
+	then
+		playerLetter='X'
+		computerLetter='0'
+	else
+		echo "Not valid choice.Try again."
+		choosePlayerLetter
+	fi
+}
+
+#To choose X or 0 by computer
+chooseComputerLetter()
+{
+	letter=$(( $RANDOM % 2 ))
+	if [ $letter -eq 1 ]
+        then
+                computerLetter='0'
+                playerLetter='X'
+        else
+                computerLetter='X'
+              	playerLetter='0'
+        fi
+}
+
 #Function to decide who will play first
 playFirstToss()
 {
@@ -24,9 +56,14 @@ playFirstToss()
 	if [ $toss -eq 1 ]
 	then
 		echo "You have won the toss.You will play first."
+		choosePlayerLetter
 	else
 		echo "Computer has won the toss.Computer will play first."
+		chooseComputerLetter
 	fi
+	echo "Letter chosen by player is : $playerLetter"
+	echo "Letter chosen by computer is : $computerLetter"
 }
 boardReset
 playFirstToss
+
