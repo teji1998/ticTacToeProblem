f8d614d95f445a66427579e8c5950441ea44ff9f
“teji1998” <tejasvinirpk@gmail.com>
2020-08-05 06:57:23 +0530

    Added:Functionality to display the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -64,6 +64,17 @@ playFirstToss()
 	echo "Letter chosen by player is : $playerLetter"
 	echo "Letter chosen by computer is : $computerLetter"
 }
+
+#To display the board
+displayBoard()
+{
+	echo " ${board[1]} | ${board[2]} | ${board[3]} "
+	echo " ----+-----+---- "
+	echo " ${board[4]} | ${board[5]} | ${board[6]} "
+        echo " ----+-----+---- "
+	echo " ${board[7]} | ${board[8]} | ${board[9]} "
+}
+
 boardReset
 playFirstToss
-
+displayBoard
